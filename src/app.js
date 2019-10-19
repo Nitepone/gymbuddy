4baedc0cf58a74b6d93444790e21f05330df70c0
Nitepone <admin@night.horse>
2019-10-18 23:30:29 -0400

    Started sample implementation of home page

--- src/app.js
+++ src/app.js
@@ -9,9 +9,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
+
+app.get('/workout', function(req, res){
+  res.render('workout.ejs');
+});
+
+app.get('/exercise', function(req, res){
+  res.render('exercise.ejs');
+});
+
+app.get('/settings', function(req, res){
+  res.render('settings.ejs');
+});
+
 
 app.listen(3000, function(){
   console.log('Server listening on port 3000');
